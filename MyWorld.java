623eac82f792e27834f06b6635023bbe37906290
raffzhm <89532144+raffzhm@users.noreply.github.com>
2022-08-09 15:24:25 +0700

    Add files via upload
    mengkoneksikan Greenfoot ke database MySql

---
+++ MyWorld.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        bee bee = new bee();
+        addObject(bee,144,131);
+        bee.setLocation(176,144);
+    }
+}
